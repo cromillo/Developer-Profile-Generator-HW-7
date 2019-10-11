64b8acf92eac863c913cfeaad475c19a3ffbe521
Claire Romillo <c_romillo@yahoo.com>
2019-10-10 21:44:13 -0700

    revised axios calls

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
+
+
 const questions = [{
   type: "input",
   message: "Enter your GitHub username:",
@@ -16,95 +18,112 @@ const questions = [{
 
 
 inquirer
-  .prompt(questions)
-
-  .then(function ({ username }) {
+.prompt(questions)
+  .then(function ({username}) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    console.log(queryUrl)
+    getUser(username);
+    getLocation(username)
+    getUserBlog(username)
+    getPublicRepo(username)
+    getStars(username)
+    getFollowers(username)
+    getFollowing(username)
+  });
 
 
-    axios.get(queryUrl).then(function (res) {
-      console.log(res.data)
-      const repoNames = res.data.map(function (repo) {
-        return repo.name;
 
-      });
+  // Get Username
 
-      module.exports = {
-        // Get Username
-        getUser: function (username) {
-          const queryUrl = 'https://api.github.com/users/${username}'
-          return axios.get(queryUrl)
-        },
+getUser = function(username){
+  const queryUrl =  `https://api.github.com/users/${username}`;
+  return axios.get(queryUrl)
+  .then(response => {
+    //console.log(response.data.name);
+  })
+}
+
 
 
   //* GET LINKS TO THE FOLLOWING:
   //Get User location via Google Maps
+  getLocation = function (username) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+    return axios.get(queryUrl)
+      .then(response => {
+        //console.log(response.data.location);
+        })
+      }
+  
+
+ 
 
   //Get User GitHub profile
 
-
+  //Get  Profile image
 
   //Get User blog
-  getUserBlog: function (username) {
-    const queryUrl = https://api.github.com/users/${username}/
+  getUserBlog = function (username) {
+    const queryUrl = `https://api.github.com/users/${username}`
     return axios.get(queryUrl)
       .then(response => {
-        return response.data.reduce((total, curr) => {
-          total += curr.blog;
-          return total
-      },
+       // console.log( response.data.blog)
+      })
+  }
+  
+
 
 
   // GET USER BIO:
- //Number of public repositories
- getPublicRepo: function (username) {
-  const queryUrl = https://api.github.com/users/${username}/
-  return axios.get(queryUrl)
-    .then(response => {
-      return response.data.reduce((total, curr) => {
-        total += curr.public_repos;
-        return total
-      },
- 
+  //Number of public repositories
+  getPublicRepo = function (username) {
+    const queryUrl = `https://api.github.com/users/${username}`
+    return axios.get(queryUrl)
+      .then(response => {
+        console.log(response.data.public_repos)   
+        }) 
+  }
 
-//Number of GitHub stars
-        getStars: function (username) {
-          const queryUrl = https://api.github.com/users/${username}/repos?per_page=100
-          return axios.get(queryUrl)
-            .then(response => {
-              return response.data.reduce((total, curr) => {
-                total += curr.stargazers_count;
-                return total
-              },
-//Number of users following
-getFollowing: function (username) {
-  const queryUrl = https://api.github.com/users/${username}
-        return axios.get(queryUrl)
-          .then(response => {
-            return response.data.following;
-          })
+
+  //Number of GitHub stars
+  getStars = function (username) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100 `
+    return axios.get(queryUrl)
+      .then(response => {
+        console.log(response.data.watchers_count)
+        })
+      
+  }
+
+  //Number of users following
+  getFollowing = function (username) {
+    const queryUrl = `https://api.github.com/users/${username}`
+    return axios.get(queryUrl)
+      .then(response => {
+        console.log(response.data.following)
+      })},
+
+  //Number of followers            
+  getFollowers =  function (username) {
+    const queryUrl = `https://api.github.com/users/${username}`
+    return axios.get(queryUrl)
+      .then(response => {
+        console.log(response.data.followers)
       })
+    }
 
-//Number of followers            
-        getFollowers: function (username) {
-            const queryUrl = https://api.github.com/users/${username}
-                  return axios.get(queryUrl)
-                    .then(response => {
-                      return response.data.followers;
-                    })
-                })
-  
 
-        const repoNamesStr = repoNames.join("\n");
 
-        fs.writeFile("repos.txt", repoNamesStr, function (err) {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-  });
-  * Profile image
+//const repoNamesStr = repoNames.join("\n");
+
+//fs.writeFile("repos.txt", repoNamesStr, function (err) {
+  //if (err) {
+    //throw err;
+  //}
+//})
+
+
+      
+
+
 
 
